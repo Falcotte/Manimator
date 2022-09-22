0a221a93872c8d3548cc7d924809e9530678c790
Ali Unal <0033484@gmail.com>
2022-09-22 18:25:58 +0300

    Play methods implemented

--- Assets/Scripts/Manimator/Manimator.cs
+++ Assets/Scripts/Manimator/Manimator.cs
@@ -1,21 +1,93 @@
 using UnityEngine;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
 
-public class Manimator : MonoBehaviour
+namespace AngryKoala.Manimator
 {
-    [SerializeField] private Animator animator;
-
-    public void Resume()
+    public class Manimator : MonoBehaviour
     {
-        animator.speed = 1f;
-    }
+        [SerializeField] private Animator animator;
 
-    public void Pause()
-    {
-        animator.speed = 0f;
-    }
+        private float normalizedTime;
 
-    public void SetSpeed(float speed)
-    {
-        animator.speed = speed;
+        public void Resume()
+        {
+            animator.speed = 1f;
+        }
+
+        public void Pause()
+        {
+            animator.speed = 0f;
+        }
+
+        public void SetSpeed(float speed)
+        {
+            animator.speed = speed;
+        }
+
+        public async void Play(string stateName)
+        {
+            DOTween.Kill($"Man{gameObject.GetInstanceID()}");
+
+            normalizedTime = 0f;
+            animator.Play(stateName, 0, 0f);
+
+            await UniTask.NextFrame();
+
+            DOTween.To(() => normalizedTime, x => normalizedTime = x, 1f, animator.GetCurrentAnimatorClipInfo(0)[0].clip.length).SetEase(Ease.Linear).SetId($"Man{gameObject.GetInstanceID()}").OnUpdate(() =>
+            {
+                animator.Play(stateName, 0, normalizedTime);
+            });
+        }
+
+        public async void Play(string stateName, float duration = -1f)
+        {
+            DOTween.Kill($"Man{gameObject.GetInstanceID()}");
+
+            normalizedTime = 0f;
+            animator.Play(stateName, 0, 0f);
+
+            await UniTask.NextFrame();
+
+            if(duration < 0)
+            {
+                DOTween.To(() => normalizedTime, x => normalizedTime = x, 1f, animator.GetCurrentAnimatorClipInfo(0)[0].clip.length).SetEase(Ease.Linear).SetId($"Man{gameObject.GetInstanceID()}").OnUpdate(() =>
+                {
+                    animator.Play(stateName, 0, normalizedTime);
+                });
+            }
+            else
+            {
+                DOTween.To(() => normalizedTime, x => normalizedTime = x, 1f, duration).SetEase(Ease.Linear).SetId($"Man{gameObject.GetInstanceID()}").OnUpdate(() =>
+                {
+                    animator.Play(stateName, 0, normalizedTime);
+                });
+            }
+        }
+
+        public async void Play(string stateName, float duration = -1f, Ease ease = Ease.Linear)
+        {
+            DOTween.Kill($"Man{gameObject.GetInstanceID()}");
+
+            normalizedTime = 0f;
+            animator.Play(stateName, 0, 0f);
+
+            await UniTask.NextFrame();
+
+            if(duration < 0)
+            {
+                DOTween.To(() => normalizedTime, x => normalizedTime = x, 1f, animator.GetCurrentAnimatorClipInfo(0)[0].clip.length).SetEase(ease).SetId($"Man{gameObject.GetInstanceID()}").OnUpdate(() =>
+                {
+                    animator.Play(stateName, 0, normalizedTime);
+                });
+            }
+            else
+            {
+                DOTween.To(() => normalizedTime, x => normalizedTime = x, 1f, duration).SetEase(ease).SetId($"Man{gameObject.GetInstanceID()}").OnUpdate(() =>
+                {
+                    animator.Play(stateName, 0, normalizedTime);
+                });
+            }
+        }
     }
 }
